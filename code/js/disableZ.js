e4b67a59824b9a38ea14e72b617dae87461855f9
cronosGP <gabrielgps1@gmail.com>
2019-06-02 18:00:46 -0300

    only organization

--- code/js/disableZ.js
+++ code/js/disableZ.js
@@ -1,3 +1,5 @@
+//working in progress
+/*
 $(document).ready(function(){
 	$(document).keydown(function(event) {
 	    	    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
@@ -17,3 +19,4 @@ $(document).ready(function(){
 	       }
 	});
 });
+*/
