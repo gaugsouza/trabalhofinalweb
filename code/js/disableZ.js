ff1007706151cd0ef305ea50b8f767b8251106f0
cronosGP <gabrielgps1@gmail.com>
2019-06-02 18:18:09 -0300

    fixed buttons border and add script to block zoom

--- code/js/disableZ.js
+++ code/js/disableZ.js
@@ -1,9 +1,7 @@
-//working in progress
-/*
+
 $(document).ready(function(){
 	$(document).keydown(function(event) {
 	    	    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
-         alert('disabling zooming'); 
 		event.preventDefault();
 		// 107 Num Key  +
 		//109 Num Key  -
@@ -14,9 +12,7 @@ $(document).ready(function(){
 
 	$(window).bind('mousewheel DOMMouseScroll', function (event) {
 	       if (event.ctrlKey == true) {
-           alert('disabling zooming'); 
 		   event.preventDefault();
 	       }
 	});
 });
-*/
